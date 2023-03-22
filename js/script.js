15cfe4a7cada839671c9a605b1928170580577b5
adriwco <adriwco@gmail.com>
2023-03-22 02:52:09 -0300

    refatoraçao

--- js/script.js
+++ js/script.js
@@ -1,27 +1,25 @@
-let current_fs, next_fs, previous_fs; //fieldsets
-let left, opacity, scale; //fieldset properties which we will animate
-let animating; //flag to quick multi-click glitches
+let current_fs, next_fs, previous_fs;
+let animating = false;
 
 $(".next").click(function () {
   if (animating) return false;
   animating = true;
-  $(this).parent().css({ transform: "scale(1)" }); // adicionado para definir a escala para 1
   current_fs = $(this).parent();
-  next_fs = $(this).parent().next();
+  next_fs = current_fs.next();
+  current_fs.css({ transform: "scale(1)" });
   $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
   next_fs.show();
   current_fs.animate(
     { opacity: 0 },
     {
       step: function (now, mx) {
-        scale = 1 - now * 0.2;
-        left = now * 50 + "%";
-        opacity = 1 - now;
+        let scale = 1 - now * 0.2;
+        let opacity = 1 - now;
         current_fs.css({
-          transform: "scale(" + scale + ")",
+          transform: `scale(${scale})`,
           position: "absolute",
         });
-        next_fs.css({ left: left, opacity: opacity.toString() });
+        next_fs.css({opacity: opacity.toString() });
       },
       duration: 800,
       complete: function () {
@@ -36,9 +34,9 @@ $(".next").click(function () {
 $(".previous").click(function () {
   if (animating) return false;
   animating = true;
-  $(this).parent().css({ transform: "scale(1)" }); // adicionado para definir a escala para 1
   current_fs = $(this).parent();
-  previous_fs = $(this).parent().prev();
+  previous_fs = current_fs.prev();
+  current_fs.css({ transform: "scale(1)" });
   $("#progressbar li")
     .eq($("fieldset").index(current_fs))
     .removeClass("active");
@@ -47,14 +45,13 @@ $(".previous").click(function () {
     { opacity: 0 },
     {
       step: function (now, mx) {
-        scale = 0.8 + (1 - now) * 0.2;
-        left = now * 50 + "%";
-        opacity = 1 - now;
+        let scale = 0.8 + (1 - now) * 0.2;
+        let opacity = 1 - now;
         current_fs.css({
-          transform: "scale(" + scale + ")",
+          transform: `scale(${scale})`,
           position: "absolute",
         });
-        previous_fs.css({ left: left, opacity: opacity.toString() });
+        previous_fs.css({opacity: opacity.toString() });
       },
       duration: 800,
       complete: function () {
